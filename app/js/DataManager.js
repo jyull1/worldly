c4a8be8cc18a3b09b58e7ab8e046f816e5dc9cbb
Joe <jyull1@ithaca.edu>
2016-10-16 16:39:42 -0400

    Attached Data Capture Event Listeners to SVG

--- app/js/DataManager.js
+++ app/js/DataManager.js
@@ -5,6 +5,31 @@ const d3 = require('d3');
 var liveData = {};
 
 module.exports = {
+
+	/**
+	Adds a vertex to the geoJSON object.
+		x (int)		The x-position of the vertex.
+		y (int)		The y-position of the vertex.
+		newPolygon (boolean)	Optional parameter that checks if a new polygon is to be created,
+								w/the vertex as it's first point.
+	*/
+	addPoint: function(x, y, newPolygon){
+		//If the vertex belongs to a new polygon within the geoJSON object
+		if(newPolygon){
+			liveData.geo.coordinates.push([[x, y]]);
+		}
+		else{
+			liveData.geo.coordinates[0].push([x, y]);
+		}
+	},
+
+	/*
+		Returns the map's data object to the frontend.
+	*/
+	getData: function(){
+		return liveData;
+	},
+
 	/**
 	Returns a blank polygon object for a new map. 
 	*/
@@ -13,12 +38,11 @@ module.exports = {
 			//A wrapper for the geoJSON object. Worldly-specific metadata must be stored outside of geo.
 			geo: {
 				type: "Polygon",
-				coordinates: []
+				coordinates: [
+					[]
+				]
 			}			
 		}
-	},
-
-	getData: function(){
-		return liveData;
 	}
+
 }
--- app/js/windows/MapEditor.js
+++ app/js/windows/MapEditor.js
@@ -1,12 +1,83 @@
 const 	d3 = require('d3'),
 	 	DataManager = require('./js/DataManager.js');
 
+var state = {
+	mode: 'ADD_VERTEX_EXISTING',
+	bgColor: '#abcdef'
+};
+
 var svg = d3.select("#chart")
 			.append('svg')
 				.attr('width', 900)
 				.attr('height', 900)
-				.style("fill", "blue");
+				.style('background', '#abcdef')
+				.on('click', processClick);
 
 var newMapButton = d3.select('#newMap');
 
 newMapButton.on('click', DataManager.newMap);
+
+/**
+Processes clicks on the SVG based on the state's mode variable
+*/
+function processClick(){
+	switch(state.mode){
+		case 'ADD_VERTEX_EXISTING':
+			addVertex(d3.mouse(this));
+			refresh();
+			break;
+		default: return;
+	}
+}
+
+/**
+Updates the SVG by fetching the DataManager copy of the object.
+*/
+function refresh(){
+    //Clears draw space
+    d3.select("svg").remove();
+
+   //Creates an SVG for my map to live in
+    var svg = d3.select("#chart")
+        .append("svg")
+        .attr("width", 900)
+        .attr("height", 900)
+        .style('background', state.bgColor)
+        .on('click', processClick);
+
+    console.log(DataManager.getData())
+
+    switch(state.mode){
+    	case 'ADD_VERTEX_EXISTING':
+    		svg.selectAll('circle')
+    			.data(DataManager.getData().geo.coordinates[0])
+    			.enter()
+    			.append('circle')
+    				.attr('r', 5)
+    				.attr('cx', function(d){
+    						return d[0];
+    					})
+    				.attr('cy', function(d){
+    						return d[1];
+    					})
+    				.attr('fill', '#000000');
+    				break;
+    	default: return;
+    }
+}
+
+/********
+
+/**
+Interprets data for the DataManager call to add a vertex to the map object.
+	coord (Array)	2-member array containing an x and y value.
+	newPolygon (boolean)	Optional boolean indicating whether the vertex will be added as a new polygon.
+*/
+function addVertex(coord, newPolygon){
+	if(newPolygon){
+		DataManager.addPoint(coord[0], coord[1], newPolygon);
+	}
+	else{
+		DataManager.addPoint(coord[0], coord[1]);
+	}
+}
